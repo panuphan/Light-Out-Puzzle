8c91d544e11d85739014a73e9ae4d9e4fc08575a
Oat <panuphan1997@gmail.com>
2017-11-16 00:16:15 +0700

    Update Grid

--- LightOut/src/Grid.java
+++ LightOut/src/Grid.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,28 +12,75 @@
  * @author User
  */
 public class Grid {
-    	private double number;
-	private double binary_num;
-	public Grid(double n, double bn)	{ number = n; binary_num = bn; }
-	public double getNumber()			    { return number; }
-	public double getBinaryNum()			{ return binary_num; }
-	public String getMessage()
-	{
-		//String s = String.format("%-9s (%.2f $)", name, bigmac);
-                String s = Double.toString(binary_num);        
-		return s;
-	}
-	public void print(int r)
-	{
-            double row = (double) r;
 
-            System.out.println("Number " + number);
-            String s = Double.toString(binary_num);
-            for (int i = 0; i < row; i++) {
-                for (int j = 0; j < row; j++) {
-                    System.out.print(s.charAt(0) + " ");
-                }
-                System.out.println("");
+    private double number;
+    private double binary_num;
+    private int[][] G;
+
+    public Grid(int[][] g) {
+            
+        G.equals(g);
+            
+    }
+
+    ;
+	public double getNumber() {
+        return number;
+    }
+
+    public double getBinaryNum() {
+        return binary_num;
+    }
+
+    public void print(int r) {
+        double row = (double) r;
+
+        System.out.println("Number " + number);
+        String s = Double.toString(binary_num);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < row; j++) {
+                System.out.print(s.charAt(0) + " ");
             }
-	}
+            System.out.println("");
+        }
+    }
+    
+    public void print(){
+        for(int i  = 0 ; i < G.length ; i++){
+            for(int j = 0 ; j <G[i].length ; j++){
+                System.out.print( G[i][j] + " " );
+            }
+            System.out.println("");
+        }
+    } 
+
+    public int[][] M(int i, int j) {
+        G[i][j]     += 1;
+        try{
+        G[i + 1][j] += 1;
+        }catch(Exception e){};
+        
+        try{
+        G[i - 1][j] += 1;
+        }catch(Exception e){};
+        
+        try{
+        G[i][j + 1] += 1;
+        }catch(Exception e){};
+        
+        try{
+        G[i][j - 1] += 1;
+        }catch(Exception e){};
+        
+        return G;
+    }
+    
+    public boolean isComplete(){
+        int[][] O = new int[G.length][G.length];
+        Arrays.fill(O, 0);
+        return Arrays.equals(G, O);
+
+    }
+    
+    
 }
--- LightOut/src/MyGraph.java
+++ LightOut/src/MyGraph.java
@@ -3,11 +3,53 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.util.*;
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.alg.*;
 
-/**
- *
- * @author User
- */
+
 public class MyGraph {
     
+    	private HashMap<Double, int[][]>    AllGrids;      // real objects
+	private ArrayList<Double>           GridNumber;		 // graph nodes
+
+	private Graph<Double, DefaultEdge>                       Graph;
+	private SimpleGraph<Double, DefaultEdge>                 SG;
+	private ConnectivityInspector<Double, DefaultEdge>       conn;
+	private KruskalMinimumSpanningTree<Double, DefaultEdge>  MST;
+        
+        
+    public MyGraph(Grid G) {
+
+        AllGrids = new HashMap<>();
+        GridNumber = new ArrayList<>();
+
+        double node = 0;
+        for (int i = 0; i < G.getGrid().length; i++) {
+            for (int j = 0; j < G.getGrid()[i].length; j++) {
+
+                AllGrids.put(node, G.M(i, j));
+                GridNumber.add(node);
+                node++;
+
+            }
+        }
+
+        SG = new SimpleGraph<Double, DefaultEdge>(DefaultEdge.class);
+        Graph = (Graph<Double, DefaultEdge>) SG;
+
+        Graphs.addAllVertices(Graph, GridNumber);
+        for (int i = 0; i < G.getGrid().length; i++) {
+            for (int j = 0; j < G.getGrid()[i].length; j++) {
+                Graph.addEdge((double)i, (double)j );
+            }
+        }
+    }
+    
+
+        
+    
 }
+
+
